e38ce08ce43719a5021b778410f93ee03d4a0506
Benjamin Andersen <benji@newbenji.dk>
2023-04-06 06:03:05 +0200

    tester

--- .gitea/scripts/tester.sh
+++ .gitea/scripts/tester.sh
@@ -58,7 +58,8 @@ lookup_changed_charts() {
     local depth=$(( $(tr "/" "\n" <<< "$charts_dir" | sed '/^\(\.\)*$/d' | wc -l) + 1 ))
     echo "depth: ${depth[*]}"
     echo ""
-    local fields="2-${depth}"
+    # local fields="2-${depth}"
+    local fields="2-"
     echo "fields: ${fields[*]}"
     echo  ""
     cut -d '/' -f "$fields" <<< "$changed_files" | uniq | filter_charts
