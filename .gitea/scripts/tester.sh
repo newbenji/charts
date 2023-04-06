fd87ab48c8774fe212e0fec5651684d75d3fc44a
Benjamin Andersen <benji@newbenji.dk>
2023-04-06 06:05:24 +0200

    tester

--- .gitea/scripts/tester.sh
+++ .gitea/scripts/tester.sh
@@ -58,7 +58,7 @@ lookup_changed_charts() {
     local depth=$(( $(tr "/" "\n" <<< "$charts_dir" | sed '/^\(\.\)*$/d' | wc -l) + 1 ))
     echo "depth: ${depth[*]}"
     echo ""
-    # local fields="2-${depth}"
+    # local fields="1-${depth}"
     local fields="2-"
     echo "fields: ${fields[*]}"
     echo  ""
